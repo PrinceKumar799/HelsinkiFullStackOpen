604525cd82be1a5b7e0647571ac8e9919ac3de06
PrinceKumar799 <77796524+PrinceKumar799@users.noreply.github.com>
2023-11-24 22:56:00 +0530

    1.9: unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,15 +11,19 @@ const Statistics = ({ good, neutral, bad })=>
   const total = good + neutral + bad;
   const average = (good - bad) / total;
   const percentagePositive = good * 100 / total;
+  const stats = <>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {total}</p>
+    <p>average {average}</p>
+    <p>positive {percentagePositive} %</p>
+  </>
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {percentagePositive} %</p>
+      {total ? stats : <p>No feedback given</p> }
+      
     </div>
   )
 
